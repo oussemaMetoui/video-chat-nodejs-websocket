6275c2d8dfff308ed7a88d85a8b1f771794255ea
oussema metoui <oussemametoui94@gmail.com>
2020-04-03 15:59:26 +0100

    enable cors

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ const rooms = require('./routes/room');
 const chat = require('./chat_namespace');
 
 app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+    res.header(
+        'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+    );
+    res.setHeader("Content-Security-Policy", "script-src 'self' *");
+    res.header('X-Content-Security-Policy', 'default-src *');
+    res.header('X-WebKit-CSP', 'default-src *');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
+});
+
 app.use(bodyParser.json());
 
 /**
