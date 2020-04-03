13f187bd7e58173862860afb6698c293f0aab3ce
oussema metoui <oussemametoui94@gmail.com>
2020-04-03 16:41:55 +0100

    add https

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const http = require('http');
+var https = require('https');
 const redis = require('socket.io-redis');
 
 const app = require('./app')
 const config = require('./config')
 
+// Credentials
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/chain.pem', 'utf8');
+let credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 // Server
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 
 // Atach server to the socket
 app.io.attach(server)
