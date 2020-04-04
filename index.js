047da5fc0f7483d5bba656250dd761ccae3f9f05
oussema metoui <oussemametoui94@gmail.com>
2020-04-04 16:48:01 +0100

    add cert credentials for production && add npm script dev

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const fs = require('fs')
 const app = require('./app')
 const config = require('./config')
 
-let credentials = {
-    key: fs.readFileSync('server.key', 'utf8'),
-    cert: fs.readFileSync('server.cert', 'utf8'),
-};
+let credentials;
+// Certificate
+if (process.env.NODE_ENV !== 'dev') {
+    const privateKey = fs.readFileSync(
+        process.env.NODE_ENV === 'dev' ? 'server.key' : '/etc/letsencrypt/live/sprint-app.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync(
+        process.env.NODE_ENV === 'dev' ? 'server.cert' : '/etc/letsencrypt/live/sprint-app.com/cert.pem', 'utf8');
+    const ca = process.env.NODE_ENV === 'dev' ? '' : fs.readFileSync(
+        process.env.NODE_ENV === 'dev' ? '' : '/etc/letsencrypt/live/sprint-app.com/chain.pem', 'utf8');
+
+    credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: process.env.NODE_ENV === 'dev' || 'staging' ? '' : ca
+    };
+}
 
 // Server
 const server = https.createServer(credentials, app);
